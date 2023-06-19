743b013eedb16e16045767d58f9a12e96cf9c869
JorgeFP97 <jcmaladiah@gmail.com>
2023-06-19 13:39:32 +0200

    css improved

--- Aladiah/Angular-App/src/app/app-routing.module.ts
+++ Aladiah/Angular-App/src/app/app-routing.module.ts
@@ -9,8 +9,7 @@ import { AboutMeComponent } from './main-page/about-me/about-me.component';
 const routes: Routes = [
   { path: 'subtheme/:id', component: SubthemeComponent, runGuardsAndResolvers: 'always'},
   { path: 'post/:id', component: PostComponent},
-  { path: 'highlighted-posts', component: AboutMeComponent},
-  { path: '', redirectTo: 'highlighted-posts', pathMatch: 'full' }
+  { path: 'about-me', component: AboutMeComponent}
 ];
 
 @NgModule({
